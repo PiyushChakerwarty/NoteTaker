e7a166cdfe3e854e1abdcf086c8e66154a088aa1
Piyush Chakerwarty <piyushchakerwarty97@gmail.com>
2021-10-11 22:16:55 +0530

    file name case correction

--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { v4: uuidv4 } = require('uuid');
 const router = express.Router();
 // const db = require('../db/db.json')
-const { readFromFile, readAndAppend, writeToFile } = require('./../helpers/fsUtils.js');
+const { readFromFile, readAndAppend, writeToFile } = require('../helpers/fsutils.js');
 router.get('/', (req, res) => {
     readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
